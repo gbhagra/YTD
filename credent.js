268577434ee82fd5bac5e29753f9e66f8f56d2c5
gbhagra <51853419+gbhagra@users.noreply.github.com>
2020-06-20 00:18:36 +0530

    Create credent.js

---
+++ credent.js
@@ -0,0 +1,24 @@
+
+module.exports = [{
+   
+    "title" : "bambiha bole ",
+    "artist" : " amrit maan"
+}, 
+{
+    "title" : "so high",
+    "artist" : "sidu mooslewala"
+
+},
+{
+    "title" : "yalgaar",
+    "artist" : "carryminati"
+},
+{
+    "title" : "ajj kal ve",
+    "artist" : "barbie maan"
+},
+{
+    "title" : "trust me",
+    "artist" : "Gurnaam bhullar"
+}
+ ]
